58958c286b3b1efd94362c1c89740ecaddc99844
Stephen Mark <stephenjmark@gmail.com>
2017-11-09 22:30:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -27,10 +27,10 @@ function doWhileLoop(array){
   function maybeTrue() {
     return Math.random() >= 0.5
   }
-
-  array
+  
   do {
     array.slice(1)
   } while (array.length > 0 && maybeTrue());
+  
   return array
 }
